611194ae6c33cf80ff4a9b42b4e76715256d9087
Michelle Steenvoorden <michellesteenvoorden93@gmail.com>
2023-01-24 14:15:12 +0100

    Add Calculate file

---
+++ src/Calculate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Calculate {
+    static int DoCalculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter a number: ");
+        int num2 = scanner.nextInt();
+
+        return num1 + num2;
+    }
+}
